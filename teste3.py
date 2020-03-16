b0908b58fba68f847e2e24792de985d18831ea9b
rammyres <rammyres@ufpi.edu.br>
2020-03-15 23:51:04 -0300

    Desenvolvimento em progresso, branch develop em atualização

--- teste3.py
+++ teste3.py
@@ -10,5 +10,13 @@ if __name__ == "__main__":
         v = Voto(str(randint(1,10)))
         x.inserir(v)
     y = MerkleTree(*x.dados())
-    print(y)
-    
+    y.export("tree.json")
+    z = MerkleTree(*x.dados())
+    z.export("tree1.json")
+
+    if y == z:
+        print("Arvore validada")
+    yy = MerkleTree.loadFromFile("tree1.json")
+    print(yy)
+    if y == yy:
+        print("Avore carregada validada")
